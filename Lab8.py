9561aa2fe2fa5b8874753615b311a982e0b83334
sanchalex <odarchenko@ex.ua>
2015-05-21 17:44:04 +0300

    Complete Labs 8. Add 2 thread. it`s worked

---
+++ Lab8.py
@@ -0,0 +1,71 @@
+# coding: utf-8
+__author__ = 'Alexander'
+
+from threading import Thread
+
+
+class List:
+    def __init__(self):
+        self.list = []
+
+    def push_back(self, elem):
+        self.list.append(elem)
+
+    def push_front(self, elem):
+        self.list.insert(0, elem)
+
+    def pop_back(self):
+        return self.list.pop()
+
+    def pop_front(self):
+        return self.list.pop(0)
+
+    def size(self):
+        return len(self.list)
+
+    def __iter__(self):
+        return self.list.__iter__()
+
+
+def clean(list):
+    i = list.size()
+    while i != 0:
+        string = list.pop_back()
+        for symbol in string:
+            if symbol.isdigit():
+                list.push_front(string)
+                i -= 1
+                break
+
+
+def cipher(list):
+    i = list.size()
+    while i != 0:
+        string = ''
+        for symbol in list.pop_back():
+            if symbol.isdigit():
+                string += str(int(symbol) * 2)
+            else:
+                string += symbol
+        list.push_front(string)
+        i -= 1
+
+
+
+if __name__ == "__main__":
+    l = List()
+    l.push_back('This string is not empty')
+    l.push_front('I have magic number: 21')
+    l.push_back('bla-bla-bla, i like number 21. Whats time? 15:31')
+
+    t1 = Thread(target=clean, args=(l,))
+    t2 = Thread(target=cipher, args=(l,))
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+
+    for x in l:
+        print x
